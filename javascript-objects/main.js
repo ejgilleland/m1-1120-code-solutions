81e60e8709da0319cb6aef4bf6d5124d12fd2654
Jared Gilleland <ejgilleland@gmail.com>
2020-12-15 22:04:15 -0800

    owo

--- javascript-objects/main.js
+++ javascript-objects/main.js
@@ -4,13 +4,24 @@ var student = {
   age: 28
 };
 
+/* an object literal is being assigned to the variable named student
+string is being assigned to the property firstName etc
+*/
+
 student.fullName = student.firstName + ' ' + student.lastName;
+/* the value stored in the firstName property of the student object
+//LEFT OF THE DOT is an OBJECT RIGHT OF THE DOT IS A PROPERTY
+object.property */
 console.log('student.fullName: ', student.fullName);
 
 student.livesInIrvine = true;
 student.previousOccupation = 'Administration';
 
 console.log('student.livesInIrvine: ', student.livesInIrvine);
+/* we are calling the log method of the console object passing two
+arguments, string and value stored within the livesInIrvine
+property of the student object
+*/
 console.log('student.previousOccupation ', student.previousOccupation);
 
 var vehicle = {
@@ -20,6 +31,7 @@ var vehicle = {
 };
 
 vehicle['color'] = 'blue-green??';
+// string blue-green?? is being assigned to the color property of the vehicle object
 vehicle['isConvertible'] = false;
 
 console.log("vehicle['color']: ", vehicle['color']);
@@ -33,5 +45,6 @@ var pet = {
 
 delete pet.name;
 delete pet['type'];
+// the delete operator with the 'name' property of the pet object
 
 console.log('variable pet: ', pet);
