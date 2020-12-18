1387def8b48209f1377b53dd1186135a9d39b5cc
Jared Gilleland <ejgilleland@gmail.com>
2020-12-17 19:44:39 -0800

    comments owo

--- javascript-arrays/main.js
+++ javascript-arrays/main.js
@@ -5,9 +5,15 @@ var colors = [
   'blue'
 ];
 
+/* array literal with the strings red, white, and blue being
+assigned to the variable colors
+*/
 console.log('value of colors[0]', colors[0]);
 console.log('value of colors[1]', colors[1]);
 console.log('value of colors[2]', colors[2]);
+
+// we are calling the log method of the console object with the arguments
+// [string] and colors at 2
 
 var america = 'America is ' + colors[0] + ', ' + colors[1] + ', and ' +
               colors[2] + '.';
@@ -28,10 +34,17 @@ var students = [
 ];
 
 var numberOfStudents = students.length;
-console.log('There are ' + numberOfStudents + ' students in the class.');
+// the value stored in the length property of the students object
+// is being assigned to the variable named numberOfStudents
 
+console.log('There are ' + numberOfStudents + ' students in the class.');
+// we are calling the log method of the console object with one argument
+// string is being concatenated with the value stored within the variable
+// numberOfStudents which is being concatenated with a string
+// & the result of that expression is being passed as the argument
 var lastIndex = numberOfStudents - 1;
 var lastStudent = students[lastIndex];
+// students at lastIndex is being assigned to the variable named lastStudent
 console.log('The last student in the array is ' + lastStudent + '.');
 
 console.log('students array: ', students);
