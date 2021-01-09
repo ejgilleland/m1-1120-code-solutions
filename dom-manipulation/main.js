225b20473744a43002d74df38caadc28f8f6f62b
Jared Gilleland <ejgilleland@gmail.com>
2021-01-09 12:06:28 -0800

    Completed requested changes (changed switch to if/else statements)

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -6,28 +6,20 @@ var clickCount = document.querySelector('.click-count');
 function hotButtonClicker() {
   clicks++;
   clickCount.textContent = 'Clicks: ' + clicks;
-  switch (true) {
-    case clicks < 4:
-      hotButton.className = 'hot-button cold';
-      break;
-    case clicks < 7:
-      hotButton.className = 'hot-button cool';
-      break;
-    case clicks < 10:
-      hotButton.className = 'hot-button tepid';
-      break;
-    case clicks < 13:
-      hotButton.className = 'hot-button warm';
-      break;
-    case clicks < 16:
-      hotButton.className = 'hot-button hot';
-      break;
-    case clicks < 25:
-      hotButton.className = 'hot-button nuclear';
-      break;
-    case clicks > 25:
-      document.querySelector('main').className = 'apocalypse';
-      break;
+  if (clicks < 4) {
+    hotButton.className = 'hot-button cold';
+  } else if (clicks < 7) {
+    hotButton.className = 'hot-button cool';
+  } else if (clicks < 10) {
+    hotButton.className = 'hot-button tepid';
+  } else if (clicks < 13) {
+    hotButton.className = 'hot-button warm';
+  } else if (clicks < 16) {
+    hotButton.className = 'hot-button hot';
+  } else if (clicks < 25) {
+    hotButton.className = 'hot-button nuclear';
+  } else if (clicks > 25) {
+    document.querySelector('main').className = 'apocalypse';
   }
 }
 
