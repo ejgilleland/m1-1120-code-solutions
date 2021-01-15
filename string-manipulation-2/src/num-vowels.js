9a5135836bf5324e1b11eb04fdcf7c808682762e
Jared Gilleland <ejgilleland@gmail.com>
2021-01-14 21:51:00 -0800

    Completed string-manipulation-2 Exercise | Jared Gilleland

---
+++ string-manipulation-2/src/num-vowels.js
@@ -0,0 +1,14 @@
+/* exported numVowels */
+function numVowels(string) {
+  var vowels = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string[i].toLowerCase() === 'a' ||
+          string[i].toLowerCase() === 'e' ||
+          string[i].toLowerCase() === 'i' ||
+          string[i].toLowerCase() === 'o' ||
+          string[i].toLowerCase() === 'u') {
+      vowels++;
+    }
+  }
+  return vowels;
+}
