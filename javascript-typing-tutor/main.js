3484aba23814cdb92a36b487d92b078e1b57356a
Jared Gilleland <ejgilleland@gmail.com>
2021-01-19 19:19:32 -0800

    Bonus JS Typing Tutor Challenge Completed

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,8 +1,15 @@
 var i = 0;
 var textList = document.querySelectorAll('span');
+var results = document.querySelector('.results');
+var accuracyResults = document.getElementById('accuracy');
+var resetButton = document.querySelector('.reset');
+var keypresses = 0;
 
 addEventListener('keydown',
   function (event) {
+    if (textList[textList.length - 1].className !== 'correct') {
+      keypresses++;
+    }
     if (i < textList.length) {
       if (textList[i].textContent === event.key) {
         textList[i].className = 'correct';
@@ -11,6 +18,23 @@ addEventListener('keydown',
           textList[i].className = 'active';
         }
       } else textList[i].className = 'incorrect';
+    }
+    if (i === textList.length && results.className !== 'results') {
+      results.className = 'results';
+      accuracyResults.textContent += Math.floor(textList.length / keypresses * 10000) / 100 + '%';
+    }
+  }
+);
+
+resetButton.addEventListener('click',
+  function (element) {
+    i = 0;
+    keypresses = 0;
+    results.className = 'results hidden';
+    accuracyResults.textContent = 'Your accuracy: ';
+    textList[0].className = 'active';
+    for (let reset = 1; reset < textList.length; reset++) {
+      textList[reset].className = '';
     }
   }
 );
